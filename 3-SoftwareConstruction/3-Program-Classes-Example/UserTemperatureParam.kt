3df7d566658e961b1cf68ff99569e4d70493670a
DimaBerny <101942300+DimaBerny@users.noreply.github.com>
2023-11-17 12:44:18 +0200

    Update UserTemperatureParam.kt

--- 3-SoftwareConstruction/3-Program-Classes-Example/UserTemperatureParam.kt
+++ 3-SoftwareConstruction/3-Program-Classes-Example/UserTemperatureParam.kt
@@ -5,8 +5,6 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserTemperatureParam(
-    val air_purifier: Boolean,
     val temperature: Int,
     val mode: String,
-    val automatic_settings: Boolean,
 ) : Parcelable
